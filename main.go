e93b16b43bf8c4380617405923908876b81c70c0
notmehul <mehulsrivatava090801@gmail.com>
2021-02-14 01:12:16 +0530

    added comments

--- main.go
+++ main.go
@@ -14,32 +14,38 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+	// using byte slices cos UTF-8 support OP
 }
 
+// calculating the hash no big dead :)
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
 
+// creating the block and calculating it's hash :D
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash}
 	block.DeriveHash()
 	return block
 }
 
+// adding the block to the chain
 func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
+	prevBlock := chain.blocks[len(chain.blocks)-1] // determines previous block in the chain
+	new := CreateBlock(data, prevBlock.Hash)       // creates the new block using the old one for hash
+	chain.blocks = append(chain.blocks, new)       // appends :)
 }
 
-func Genesis() *Block {
+// adds the first block to the chain :3
+func Init() *Block {
 	return CreateBlock("INIT", []byte{})
 }
 
+// the madlad which started everything
 func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+	return &BlockChain{[]*Block{Init()}}
 }
 
 func main() {
